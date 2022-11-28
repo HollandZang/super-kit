26062a011ef73e25b7b0552c2179cf781e35d306
ZhangHN <zhn.pop@gmail.com>
2022-11-28 14:49:05 +0800

    update: DataBaseManager.java

--- db/src/main/java/com/holland/kit/db/tenant/DataBaseManager.java
+++ db/src/main/java/com/holland/kit/db/tenant/DataBaseManager.java
@@ -6,40 +6,47 @@ import com.holland.kit.base.file.YamlKit;
 import com.holland.kit.base.functional.Either;
 import com.holland.kit.base.log.ILog;
 import com.holland.kit.base.log.LogFactory;
+import com.holland.kit.db.JDBCPool;
 import com.holland.kit.db.MysqlPool;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MysqlManager {
-    private static final ILog log = LogFactory.create(MysqlManager.class);
+public class DataBaseManager {
+    private static final ILog log = LogFactory.create(DataBaseManager.class);
 
     private static final String DEFAULT_KEY = "DEFAULT_KEY";
 
-    private static volatile MysqlManager instance;
+    private static volatile DataBaseManager instance;
 
-    public final Map<String, MysqlPool> pools;
+    public final Map<String, JDBCPool> pools;
 
-    private MysqlManager(int initialCapacity) {
+    private DataBaseManager(int initialCapacity) {
         this.pools = new HashMap<>(initialCapacity);
     }
 
-    public static MysqlManager getInstance() {
+    public static DataBaseManager getInstance() {
         if (instance == null) {
-            synchronized (MysqlManager.class) {
+            synchronized (DataBaseManager.class) {
                 if (instance == null) {
                     YamlKit.getInstance().read(".", "datasource.yml", false)
                             .then(read -> {
                                 JsonX confList = new JsonX(read.get("com.holland.kit.db"));
                                 int   size     = ((JSONArray) confList.resource).size();
-                                instance = new MysqlManager(size);
+                                instance = new DataBaseManager(size);
                                 for (int i = 0; i < size; i++) {
                                     Map<String, Object> conf = confList.find("[" + i + "]");
                                     String              key  = conf.getOrDefault("key", DEFAULT_KEY).toString();
                                     if (instance.pools.containsKey(key))
                                         return Either.error(new RuntimeException("Duplicate key: " + key));
-                                    instance.pools.put(key, new MysqlPool(conf));
+
+                                    Object driverClassName = conf.get("driverClassName");
+                                    if ("com.mysql.cj.jdbc.Driver".equals(driverClassName) || "com.mysql.jdbc.Driver".equals(driverClassName)) {
+                                        instance.pools.put(key, new MysqlPool(conf));
+                                    } else {
+                                        return Either.error(new RuntimeException("Not support driver: " + driverClassName));
+                                    }
                                 }
                                 return Either.success(instance);
                             })
@@ -54,7 +61,7 @@ public class MysqlManager {
         return instance;
     }
 
-    public MysqlPool use(String key) {
+    public JDBCPool use(String key) {
         return pools.get(key);
     }
 
